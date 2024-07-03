be901d2d47d9fa5b26dea34009f97fcb0f265160
souravrs999 <souravraveendran6@gmail.com>
2024-07-03 16:40:27 +0530

    kiran testimonial

--- data/testimonials.ts
+++ data/testimonials.ts
@@ -11,8 +11,8 @@ export const testimonialsData = [
     id: 2,
     author: "Kiran Joshua Baby",
     designation: "Project Lead",
-    image: "/images/testimonial-surag-avatar.jpg",
+    image: "/images/testimonial-kiran-avatar.jpg",
     content:
-      "Sourav is an exceptionally skilled frontend developer with a deep expertise in React. His ability to deliver high-quality, responsive, and user-friendly interfaces on time is remarkable, consistently exceeding project expectations and contributing significantly to our team's success.",
+      "I had the pleasure of working with Sourav as both his team lead and colleague on a significant project. Sourav consistently demonstrated exceptional skills in front-end technologies such as React, Node, and Next.js. His ability to grasp new concepts and technologies quickly is remarkable, making him an invaluable asset to our team.",
   },
 ];
